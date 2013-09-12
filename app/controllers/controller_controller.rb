0a1fa486bb017f7a5bbe7f04a8dc536ac582fdfd
Jacob Martin <jmart.alb@gmail.com>
2013-09-12 12:52:18 -0400

    Initial Rails Deploy

---
+++ app/controllers/controller_controller.rb
@@ -0,0 +1,7 @@
+class ControllerController < ApplicationController
+  def welcome
+  end
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paytheartist::Application.config.secret_key_base = '65fd541351f98500e142df4098c23ea6e1b93afaa25330357b6bc55085e8c68c79af8a0b02b9e2313fc62742e8624b7c6e06eef1241d686a019595e80874fa0a'
