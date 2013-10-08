33d591dc21a15cb0596b533ae61d93830bd20fa9
Jacob Martin <jmart.alb@gmail.com>
2013-10-07 22:46:13 -0400

    Initial Bootstrap Commits

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,28 @@
+﻿  $(function () {
+
+	  //to be changed to bootsrap syntax
+		$(".Mission-header").click(function(){
+			$(".Mission").fadeIn();
+		}); 
+		$(".About-header").click(function(){
+			$(".About").fadeIn();
+		}); 
+		$(".FAQ-header").click(function(){
+			$(".FAQ").fadeIn();
+		}); 	
+		$(".Contact-header").click(function(){
+			$(".Contact").fadeIn();
+		}); 
+		$(".Legal-header").click(function(){
+			$(".Legal").fadeIn();
+		}); 
+		$(".close").click(function(){
+			$(".Mission").fadeOut();
+			$(".About").fadeOut();
+			$(".FAQ").fadeOut();
+			$(".Contact").fadeOut();
+			$(".Legal").fadeOut();
+			//this can be done a lot easier.
+		}); 					
+
+});
